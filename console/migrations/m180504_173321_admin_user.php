44d9bc8812128bdf06856d7904de1f3765d03c16
Sinutkamixey <Sinutkamixey@Sinutkamixey.ru>
2018-05-30 16:12:42 +0700

    Правка миграции 2

--- console/migrations/m180504_173321_admin_user.php
+++ console/migrations/m180504_173321_admin_user.php
@@ -18,8 +18,8 @@ class m180504_173321_admin_user extends Migration
             'password_hash' => Yii::$app->getSecurity()->generatePasswordHash('qwerty'),
             'auth_key' => '',
             'status' => '20',
-            'created_at' => '',
-            'updated_at' => '',
+            'created_at' => time(),
+            'updated_at' => time(),
         ]);
     }
 
